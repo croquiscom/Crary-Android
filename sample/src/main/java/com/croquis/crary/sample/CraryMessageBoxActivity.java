88bfe1438a1dc72bdcd65b945c3c6447e18b9b76
yamigo <yamigo1021@gmail.com>
2014-09-30 12:11:08 +0900

    Dialog - modify sample of CraryMessageBox

--- sample/src/main/java/com/croquis/crary/sample/CraryMessageBoxActivity.java
+++ sample/src/main/java/com/croquis/crary/sample/CraryMessageBoxActivity.java
@@ -5,7 +5,6 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
-import android.view.View.OnClickListener;
 
 import com.croquis.crary.dialog.CraryMessageBox;
 
@@ -14,60 +13,22 @@ public class CraryMessageBoxActivity extends ActionBarActivity{
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.crary_message_box);
+	}
 
-		findViewById(R.id.alert).setOnClickListener(new OnClickListener() {
+	public void onShowAlert(View view) {
+		CraryMessageBox.alert(this, "message", "title", new DialogInterface.OnClickListener() {
 			@Override
-			public void onClick(View view) {
-				CraryMessageBox.alert(CraryMessageBoxActivity.this, "alert");
+			public void onClick(DialogInterface dialogInterface, int which) {
+				CraryMessageBox.alert(CraryMessageBoxActivity.this, "done");
 			}
 		});
+	}
 
-		findViewById(R.id.confirm).setOnClickListener(new OnClickListener() {
+	public void onShowConfirm(View view) {
+		CraryMessageBox.confirmYesNo(this, "Really?", new DialogInterface.OnClickListener() {
 			@Override
-			public void onClick(View view) {
-				CraryMessageBox.confirm(CraryMessageBoxActivity.this, "confirm", "Yes", "No", new DialogInterface.OnClickListener() {
-					@Override
-					public void onClick(DialogInterface dialogInterface, int which) {
-						if (which == DialogInterface.BUTTON_POSITIVE) {
-							// do something
-						}
-
-						dialogInterface.dismiss();
-					}
-				});
-			}
-		});
-
-		findViewById(R.id.confirmOkCancel).setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View view) {
-				CraryMessageBox.confirmOkCancel(CraryMessageBoxActivity.this, "confirmOkCancel", new DialogInterface.OnClickListener() {
-					@Override
-					public void onClick(DialogInterface dialogInterface, int which) {
-						if (which == DialogInterface.BUTTON_POSITIVE) {
-							// do something
-						}
-
-						dialogInterface.dismiss();
-					}
-				});
-			}
-		});
-
-		findViewById(R.id.confirmYesNo).setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View view) {
-				CraryMessageBox.confirmYesNo(CraryMessageBoxActivity.this, "confirmYesNo", new DialogInterface.OnClickListener() {
-					@Override
-					public void onClick(DialogInterface dialogInterface, int which) {
-						if (which == DialogInterface.BUTTON_POSITIVE) {
-							// do something
-						}
-
-						dialogInterface.dismiss();
-					}
-				});
-
+			public void onClick(DialogInterface dialogInterface, int which) {
+				CraryMessageBox.alert(CraryMessageBoxActivity.this, which == DialogInterface.BUTTON_POSITIVE ? "You confirmed" : "You cancelled");
 			}
 		});
 	}
